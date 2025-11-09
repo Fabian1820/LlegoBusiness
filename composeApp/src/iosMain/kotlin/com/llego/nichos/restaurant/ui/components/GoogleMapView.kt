2f5df83a52edce545a178d902db3f20b57f09efe
Ruben0304 <hernandzruben9@gmail.com>
2025-11-08 19:13:43 -0500

    ruben ios

---
+++ composeApp/src/iosMain/kotlin/com/llego/nichos/restaurant/ui/components/GoogleMapView.kt
@@ -0,0 +1,12 @@
+package com.llego.nichos.restaurant.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+actual fun BusinessLocationMap(
+    onLocationSelected: (Double, Double) -> Unit,
+    modifier: Modifier
+) {
+    // TODO: Implement GoogleMapView for iOS
+}
---
+++ composeApp/src/nativeMain/kotlin/com/llego/nichos/restaurant/ui/components/GoogleMapView.native.kt
@@ -0,0 +1,2 @@
+package com.llego.nichos.restaurant.ui.components 
+
